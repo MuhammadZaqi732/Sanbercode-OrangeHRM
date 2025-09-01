7319dae5de9c80954b6598d21f467cc4cc7d5855
Muhammad Zaqi <153579623+MuhammadZaqi732@users.noreply.github.com>
2025-09-01 23:12:02 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,9 +5,12 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+     screenshotOnRunFailure: true, // default: true
+     video: true,                  // default: true
+
     supportFile: "cypress/support/e2e.js",
     pageLoadTimeout: 120000,
     defaultCommandTimeout: 30000,
-    baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php"
+    baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php",
   },
 });
